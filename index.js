04c143a2092f349cdabf2fe953698232a78eb5f8
D3NCyan <edwarddoltons@gmail.com>
2021-11-02 22:46:21 +0800

    add maintenence path system

--- index.js
+++ index.js
@@ -7,6 +7,13 @@ const port = 8080
 
 app.use(express.json())
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH')
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+    next()
+})
+
 app.use('/api/v1', router)
 
 app.use('/uploads', express.static('uploads'))
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,13 @@
 const express = require('express') 
 const { getCountrys, addCountry, getCountry, updateCountry, deleteCountry } = require('../controllers/country') 
-const { getTrips, addTrip, detailTrip, deleteTrip } = require('../controllers/trip') 
+const { getTrips, addTrip, detailTrip, deleteTrip, updateTrip } = require('../controllers/trip') 
 const { addTransaction, getTransactions, deleteTransaction, updateTransaction } = require('../controllers/transaction') 
 const { getUsers, deleteUser, updateUser, getUser } = require('../controllers/user') 
 const { auth } = require('../middlewares/auth') 
 const { register, login } = require('../controllers/auth') 
 const { checkAdmin } = require('../middlewares/checkAdmin')  
 const { uploadFile } = require('../middlewares/uploadFIle')  
+const { maintenceDeleteTrips } = require('../testing/maintenceDeleteTrips')
 
 const router = express.Router() 
 
@@ -26,13 +27,15 @@ router.delete('/country/:id', auth, checkAdmin, deleteCountry)
 
 router.get('/trips', getTrips) 
 router.post('/trip', auth, checkAdmin, uploadFile("image", 'uploads/trips'), addTrip) 
+router.patch('/trip/:id', uploadFile("image", 'uploads/trips'), updateTrip)
 router.get('/trip/:id', detailTrip) 
 router.delete('/trip/:id', auth, checkAdmin, deleteTrip) 
 
-
 router.post('/transaction', uploadFile("attachment", 'uploads/proof'), addTransaction) 
-router.patch('/transaction/:id', updateTransaction) // need watched
-router.delete('/transaction/:id',  deleteTransaction) 
+router.patch('/transaction/:id', auth, uploadFile("attachment", 'uploads/proof'), updateTransaction) 
+router.delete('/transaction/:id', auth,  deleteTransaction) 
 router.get('/transactions', auth, checkAdmin, getTransactions) 
+
+router.delete('/maintence/trips', maintenceDeleteTrips) 
 
 module.exports = router
